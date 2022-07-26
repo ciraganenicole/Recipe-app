b5dbdd653a80ede200a50bd23abbd9edab9b195e
ciraganenicole <ciraganenicole@gmail.com>
2022-07-26 20:01:14 +0200

    Add description column to inventory table

--- db/migrate/20220725192718_create_inventories.rb
+++ db/migrate/20220725192718_create_inventories.rb
@@ -4,6 +4,7 @@ class CreateInventories < ActiveRecord::Migration[7.0]
   def change
     create_table :inventories do |t|
       t.string :name
+      t.text :description
       t.timestamps
     end
   end
