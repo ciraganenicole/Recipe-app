2a0d9e8a562d14b6ee4a34c2c4499cb978762762
ciraganenicole <ciraganenicole@gmail.com>
2022-07-28 07:44:03 +0200

    Add shopping list feature

---
+++ app/models/inventory_food.rb
@@ -0,0 +1,6 @@
+class InventoryFood < ApplicationRecord
+    validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  
+    belongs_to :inventory
+    belongs_to :food
+end
