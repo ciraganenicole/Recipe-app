8db538712b7aa31871c48de64fa74d23c1eee15b
ciraganenicole <ciraganenicole@gmail.com>
2022-07-28 07:51:31 +0200

    Fix linters errors

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,17 +1,17 @@
 class ShoppingListsController < ApplicationController
-    def index
-        @all_foods = current_user
-          .recipes
-          .includes([:recipe_foods])
-          .joins('INNER JOIN recipe_foods ON recipe_foods.recipe_id = recipes.id')
-          .joins('INNER JOIN foods ON recipe_foods.food_id = foods.id')
-          .select("foods.name as name,
+  def index
+    @all_foods = current_user
+      .recipes
+      .includes([:recipe_foods])
+      .joins('INNER JOIN recipe_foods ON recipe_foods.recipe_id = recipes.id')
+      .joins('INNER JOIN foods ON recipe_foods.food_id = foods.id')
+      .select("foods.name as name,
                 foods.measurement_unit as measurement_unit,
                 sum(recipe_foods.quantity) as quantity,
                 sum(foods.price * recipe_foods.quantity) as price")
-          .group('foods.id')
-    
-        @food_count = @all_foods.length
-        @total_price = @all_foods.reduce(0) { |total, food| total + food[:price] }
-      end
+      .group('foods.id')
+
+    @food_count = @all_foods.length
+    @total_price = @all_foods.reduce(0) { |total, food| total + food[:price] }
+  end
 end
