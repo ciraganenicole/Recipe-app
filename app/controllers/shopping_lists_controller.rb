e9c7d37840fd7387cf4e8dae33a6ddc233999dad
Achilles-Dev <solohag92@gmail.com>
2022-07-29 16:01:53 +0000

    Update shopping_list controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -3,16 +3,21 @@ class ShoppingListsController < ApplicationController
 
   def index
     @recipe = Recipe.includes(:foods).find(params[:recipe_id])
+    redirect_to inventories_path if Inventory.first.nil?
     @inventory = Inventory.includes(:foods).find(params[:inventory_id])
-    @all_foods = []
-    p @all_foods
-    @recipe.foods.each do |recipe_food|
-      @inventory.foods.each do |inventory_food|
-        @all_foods.push(recipe_food) unless recipe_food.name == inventory_food.name
-        p recipe_food.recipe_foods.where(food_id: recipe_food.id).pluck('quantity')
+    if @inventory.foods.first.nil?
+      @all_foods = @recipe.foods
+    else
+      @all_foods = []
+      @recipe.foods.each do |recipe_food|
+        @inventory.foods.each do |inventory_food|
+          @all_foods.push(recipe_food) unless recipe_food.name == inventory_food.name
+        end
       end
     end
     @food_count = @all_foods.length
-    @total_price = @all_foods.reduce(0) { |total, food| total + food[:price] }
+    @total_price = @all_foods.reduce(0) do |total, food|
+      total + (food[:price] * food.recipe_foods.where(food:).pluck('quantity')[0])
+    end
   end
 end
